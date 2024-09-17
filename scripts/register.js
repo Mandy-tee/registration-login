447cafcd2edf8308cae5905104515cf961e19ae7
Amanda Tagoe <nayork77@gmail.com>
2024-09-17 15:52:07 +0000

    Updated Js

--- scripts/register.js
+++ scripts/register.js
@@ -1,7 +1,22 @@
 // Get form element by id
-const registerForm = document.getElementById('register');
+const registerForm = document.querySelector("#register");
 // Handle form submit event
-registerForm.onsubmit = function(event) {
-    event.preventDefault();
-    console.log(event);
+registerForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  // Collect form data
+  const formData = new FormData(registerForm);
+  // Send data to backend
+  //Display success message to user
+  const messageH1 = document.querySelector('#message');
+  messageH1.textContent = 'Account registered successfully!';
+});
+
+// Declare variable age
+const age = 71;
+// Define squareAge function
+function squareAge(age) {
+  return age ** 2;
 }
+
+const squaredAge = squareAge(34);
+console.log(squaredAge);
